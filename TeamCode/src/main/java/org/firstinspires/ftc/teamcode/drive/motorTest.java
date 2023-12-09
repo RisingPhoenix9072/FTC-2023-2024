a5d43f229017ecd2180f0341908be1dcd7ff88dd
RisingPhoenix9072 <61521648+RisingPhoenix9072@users.noreply.github.com>
2023-12-09 13:13:34 -0600

    Started up the code and made edits to specialise it for 9072 robot -Auralia(9027)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/motorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/motorTest.java
@@ -18,45 +18,31 @@ import org.firstinspires.ftc.robotcore.internal.camera.delegating.DelegatingCapt
 
 public class motorTest extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor frontLeft = null;
-    private DcMotor frontRight = null;
-    private DcMotor backLeft = null;
-    private DcMotor backRight = null;
-    private DcMotor motor5 = null;
-    private DcMotor Dronatron;
+    private DcMotor left = null;
+    private DcMotor right = null;
+    private DcMotor Arm;
     private Servo servo1;
-    private Servo servo2;
 
     @Override
     public void runOpMode() {
 
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
-        frontLeft = hardwareMap.get(DcMotor.class, "motor1");
-        frontRight = hardwareMap.get(DcMotor.class, "motor2");
-        backLeft = hardwareMap.get(DcMotor.class, "motor3");
-        backRight = hardwareMap.get(DcMotor.class, "motor4");
-//        motor5 = hardwareMap.get(DcMotor.class, "armMotor");
-        Dronatron = hardwareMap.get(DcMotor.class, "motor6");
-//        servo1 = hardwareMap.get(Servo.class, "pinch");
-//        servo2 = hardwareMap.get(Servo.class, "creeeak");
+        left = hardwareMap.get(DcMotor.class, "motor1");
+        right = hardwareMap.get(DcMotor.class, "motor2");
+        Arm = hardwareMap.get(DcMotor.class, "motor3");
+        servo1 = hardwareMap.get(Servo.class, "tophat");
 
-        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
+        left.setDirection(DcMotor.Direction.FORWARD);
+        right.setDirection(DcMotor.Direction.REVERSE);
 
-        frontLeft.setDirection(DcMotor.Direction.FORWARD);
-        frontRight.setDirection(DcMotor.Direction.REVERSE);
-        backLeft.setDirection(DcMotor.Direction.FORWARD);
-        backRight.setDirection(DcMotor.Direction.REVERSE);
 
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
@@ -67,11 +53,11 @@ public class motorTest extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            
+            Arm.setPower(0.1);
+            Arm.setPower(Math.pow(gamepad2.left_stick_x, 3));
 
-//            motor5.setPower(0.1);
-//            motor5.setPower(Math.pow(gamepad2.left_stick_x, 3));
-
-            double max;
+            double max = 0;
 
             double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
             double lateral =  gamepad1.left_stick_x;
@@ -89,56 +75,37 @@ public class motorTest extends LinearOpMode {
             }
                 // Combine the joystick requests for each axis-motion to determine each wheel's power.
             // Set up a variable for each drive wheel to save the power level for telemetry.
-            double leftFrontPower  = axial + lateral + yaw;
-            double rightFrontPower = axial - lateral - yaw;
             double leftBackPower   = axial - lateral + yaw;
             double rightBackPower  = axial + lateral - yaw;
 
             // Normalize the values so no wheel power exceeds 100%
             // This ensures that the robot maintains the desired motion.
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
             max = Math.max(max, Math.abs(leftBackPower));
             max = Math.max(max, Math.abs(rightBackPower));
 
             if (max > 1.0) {
-                leftFrontPower  /= max;
-                rightFrontPower /= max;
                 leftBackPower   /= max;
                 rightBackPower  /= max;
             }
 
-            frontLeft.setPower(Math.pow(leftFrontPower, 3));
-            frontRight.setPower(Math.pow(rightFrontPower, 3));
-            backLeft.setPower(Math.pow(leftBackPower, 3));
-            backRight.setPower(Math.pow(rightBackPower, 3));
+            left.setPower(Math.pow(leftBackPower, 3));
+            right.setPower(Math.pow(rightBackPower, 3));
 
-            int position1 = frontLeft.getCurrentPosition();
-            int position2 = frontRight.getCurrentPosition();
-            int position3 = backLeft.getCurrentPosition();
-            int position4 = backRight.getCurrentPosition();
+            int position1 = left.getCurrentPosition();
+            int position2 = right.getCurrentPosition();
 
-//            if (gamepad2.x){
-//                servo1.setPosition(0);
-//            }
-//            if (gamepad2.y){
-//                servo1.setPosition(1);
-//            }
-//            if (gamepad2.a) {
-//                servo2.setPosition(0);
-//            }
-//            if (gamepad2.b) {
-//                servo2.setPosition(1);
-//            }
-            Dronatron.setPower(-gamepad2.right_trigger);
+            if (gamepad2.x){
+                servo1.setPosition(0);
+            }
+            if (gamepad2.y){
+                servo1.setPosition(1);
+            }
 
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
             telemetry.addData("Encoder 1 Position", position1);
             telemetry.addData("Encoder 2 Position", position2);
-            telemetry.addData("Encoder 3 position", position3);
-            telemetry.addData("Encoder 4 position", position4);
             telemetry.update();
         }
     }
